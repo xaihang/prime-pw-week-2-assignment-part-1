be8ba4a97f9a5fd97b5335f2cdb4d4db3ab7c29f
Xai Hang <xaihang12@gmail.com>
2022-12-05 13:06:25 -0600

    created var & conditional statement

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -1,34 +1,41 @@
 // REQUIRED FEATURES:
 // 1 - Create a variable called `firstName` and assign it the value of your first name
-
+const firstName = 'Xai';
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
-
+const lastName = 'Hang';
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 // (remember, you already have variables for this, can you use those?)
-
+const fullName = firstName.concat(lastName);
 // 4 - Console log the value of `fullName`
-
+console.log(fullName); // Xai Hang
 // 5 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
-
+let luckyNumber = 55;
 // 6 - Console log this sentence, adding in the variables you created above: 
 // 'My name is (full name), and I think (lucky number) is a winner!'.
 // Refer back to the videos if you need help with this one.
-
+console.log(`My name is ${fullName}, and I think ${luckyNumber} is a winner!`)
 // 7 - Create a variable named `adventurous` and set it to a boolean value (true or false)
-
+let adventurous = true;
 // 8 - Create a variable named `food`, and set its value to a string of your favorite food
-
+let food = 'anything bacon!';
 // 9 - Create a variable called `pets` and set it to the value of the number of pets you have
-
+let pets = 2;
 // 10 - Create a variable called `friendsPets` and assign it the value of the number of pets your friend has
-
+let friendsPets = 2;
 // 11 - Add two pets to your `pets` variable
-
+pets += 2;
 // 12 - Create a constant variable called `allowedPets` and set it to a number value of your choice
-
+const allowedPets = 2;
 // 13 - Create a conditional: if adventurous is true, console log "Adventures are great!", 
 // if it's not true, console log "How about we stay home?"
-
+function adventure() {
+    if (adventurous === true) {
+        console.log ('Adventures are great!');
+    } else {
+        console.log ('How about we stay home?');
+    }
+}
+adventure();
 
 // 14 - Create a compound conditional: if luckyNumber is 2 and adventurous is true,
 // console log "Roll the dice!"
