38a0ffd6b28e4ee5f0a6132ab730f686bb7c3c70
Xai Hang <xaihang12@gmail.com>
2022-12-11 13:11:09 -0600

    created fullName variable with template literal/string method

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -5,7 +5,8 @@ const firstName = 'Xai';
 const lastName = 'Hang';
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 // (remember, you already have variables for this, can you use those?)
-const fullName = firstName + ' ' + lastName;
+// const fullName = firstName + ' ' + lastName; //the operator method of combing variables 
+const fullName = (`${firstName} ${lastName}`); // Template literals or template strings 
 // 4 - Console log the value of `fullName`
 console.log(fullName); // Xai Hang
 // 5 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
@@ -13,7 +14,7 @@ let luckyNumber = 2;
 // 6 - Console log this sentence, adding in the variables you created above:
 // 'My name is (full name), and I think (lucky number) is a winner!'.
 // Refer back to the videos if you need help with this one.
-console.log(`My name is ${fullName}, and I think ${luckyNumber} is a winner!`);
+console.log(`My name is ${fullName}, and I think ${luckyNumber} is a winner!`); //using template literals/template string method
 // 7 - Create a variable named `adventurous` and set it to a boolean value (true or false)
 let adventurous = true;
 // 8 - Create a variable named `food`, and set its value to a string of your favorite food
